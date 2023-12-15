2b2145429ec6b9af0e78d5492689c841d3fa289b
Dhruv Patel <dhruv.patel@pdisoftware.com>
2023-12-15 14:58:31 -0600

    feat: Demonstrate use of multiple typedefs and dataSources

--- index.js
+++ index.js
@@ -2,45 +2,47 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const bodyParser = require('body-parser');
-const { mergeResolvers } = require('@graphql-tools/merge');
+const { mergeResolvers, mergeTypeDefs } = require('@graphql-tools/merge');
 
-const typeDefs = `#graphql
+const personTypeDefs = `#graphql
   type Person {
-    id: ID!
-    name: String!
-  }
-
-  type Station {
     id: ID!
     name: String!
   }
 
   type Query {
     getListOfPersons: [Person!]!
-    getStations: [Station!]!
   }
 `;
 
 const personResolver = {
   Query: {
-    getListOfPersons: (parent, args, context) => ([
-      { id: 1, name: 'Person1' },
-      { id: 2, name: 'Person2' },
-      { id: 3, name: 'Person3' },
-    ]),
+    getListOfPersons: (parent, args, context) => {
+      return context.dataSources.person.getAll();
+    },
   },
 };
 
+const stationTypeDefs = `#graphql
+  type Station {
+    id: ID!
+    name: String!
+  }
+
+  type Query {
+    getStations: [Station!]!
+  }
+`;
+
 const stationResolver = {
   Query: {
-    getStations: (parent, args, context) => ([
-      { id: 1, name: 'Station1' },
-      { id: 2, name: 'Station2' },
-      { id: 3, name: 'Station3' },
-    ]),
+    getStations: (parent, args, context) => {
+      return context.dataSources.stations.getAll();
+    },
   },
 }
 
+const typeDefs = mergeTypeDefs([personTypeDefs, stationTypeDefs]);
 const resolvers = mergeResolvers([personResolver, stationResolver]);
 
 const server = new ApolloServer({
@@ -48,7 +50,26 @@ const server = new ApolloServer({
   resolvers,
 });
 
-const context = async ({ req }) => ({});
+const dataSources = {
+  person: {
+    getAll: () => [
+      { id: 1, name: 'Person1' },
+      { id: 2, name: 'Person2' },
+      { id: 3, name: 'Person3' },
+    ],
+  },
+  stations: {
+    getAll: () => [
+      { id: 1, name: 'Station1' },
+      { id: 2, name: 'Station2' },
+      { id: 3, name: 'Station3' },
+    ],
+  },
+};
+
+const context = async ({ req }) => ({
+  dataSources,
+});
 
 async function runApp() {
   const app = express();
@@ -59,7 +80,7 @@ async function runApp() {
 
   await server.start();
 
-  app.use('/graphql', bodyParser.json(), expressMiddleware(server));
+  app.use('/graphql', bodyParser.json(), expressMiddleware(server, { context }));
 
   app.listen(8000, () => {
     console.log('App is listening on port 8000...');
