dc538945550756e3d7caef63d231127a27d15dea
Dhruv Patel <dhruv.patel@pdisoftware.com>
2023-12-15 14:20:45 -0600

    Initialize repo

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const bodyParser = require('body-parser');
+
+const typeDefs = `#graphql
+  type Person {
+    id: ID!
+    name: String!
+  }
+
+  type Station {
+    id: ID!
+    name: String!
+  }
+
+  type Query {
+    getListOfPersons: [Person!]!
+    getStations: [Station!]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getListOfPersons: (parent, args, context) => ([
+      { id: 1, name: 'Person1' },
+      { id: 2, name: 'Person2' },
+      { id: 3, name: 'Person3' },
+    ]),
+    getStations: (parent, args, context) => ([
+      { id: 1, name: 'Station1' },
+      { id: 2, name: 'Station2' },
+      { id: 3, name: 'Station3' },
+    ]),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const context = async ({ req }) => ({});
+
+async function runApp() {
+  const app = express();
+
+  app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
+  });
+
+  await server.start();
+
+  app.use('/graphql', bodyParser.json(), expressMiddleware(server));
+
+  app.listen(8000, () => {
+    console.log('App is listening on port 8000...');
+  });
+}
+
+runApp();
